535e01d88cf7c3497f9f31d646a6afaba55f0716
Ryan Schmukler <rschmukler@gmail.com>
2016-01-30 18:39:21 -0500

    feat(grabbag): add initial core functionality and tests

--- grabbag.go
+++ grabbag.go
@@ -1,15 +1,105 @@
 package grabbag
 
-import (
-	"strings"
-)
+import "strings"
+
+// Seperator is the seperator used by grabbag to traverse
+var Seperator = "."
 
 // GrabBag is the only type in GrabBag. It is an API to traverse nested map data
 type GrabBag struct {
-	data map[string]interface{}
+	data interface{}
 }
 
-// Creates a new GrabBag from raw data
-func FromData(data map[string]interface{}) *GrabBag {
+// FromData creates a new GrabBag from raw data
+func FromData(data interface{}) *GrabBag {
 	return &GrabBag{data}
+}
+
+// Grab gets a generic interface from our data
+func (g *GrabBag) Grab(query string) interface{} {
+	levels := strings.Split(query, Seperator)
+	path := levels[0]
+	if asMap, ok := g.data.(map[string]interface{}); ok {
+		if len(levels) > 1 {
+			return FromData(asMap[path]).Grab(strings.Join(levels[1:], Seperator))
+		}
+		return asMap[path]
+	}
+	return nil
+}
+
+// Has returns whether our data has the selected query available
+func (g *GrabBag) Has(query string) bool {
+	levels := strings.Split(query, Seperator)
+	path := levels[0]
+	if asMap, ok := g.data.(map[string]interface{}); ok {
+		if len(levels) > 1 {
+			return FromData(asMap[path]).Has(strings.Join(levels[1:], Seperator))
+		}
+		return asMap[path] != nil
+	}
+	return false
+}
+
+// String grabs a query casted as a string
+func (g *GrabBag) String(query string) string {
+	res := g.Grab(query)
+	if res != nil {
+		return res.(string)
+	}
+	return ""
+}
+
+// StringSlice grabs a query casted as a []string
+func (g *GrabBag) StringSlice(query string) []string {
+	res := g.Grab(query)
+	if res != nil {
+		return res.([]string)
+	}
+	return []string{}
+}
+
+// Int grabs a query casted as an int
+func (g *GrabBag) Int(query string) int {
+	res := g.Grab(query)
+	if res != nil {
+		return res.(int)
+	}
+	return 0
+}
+
+// IntSlice grabs a query casted as an []int
+func (g *GrabBag) IntSlice(query string) []int {
+	res := g.Grab(query)
+	if res != nil {
+		return res.([]int)
+	}
+	return []int{}
+}
+
+// Bool grabs a query casted as a bool
+func (g *GrabBag) Bool(query string) bool {
+	res := g.Grab(query)
+	if res != nil {
+		return res.(bool)
+	}
+	return false
+}
+
+// Float32 grabs a query casted as a float32
+func (g *GrabBag) Float32(query string) float32 {
+	res := g.Grab(query)
+	if res != nil {
+		return res.(float32)
+	}
+	return float32(0)
+}
+
+// Float64 grabs a query casted as a float64
+func (g *GrabBag) Float64(query string) float64 {
+	res := g.Grab(query)
+	if res != nil {
+		return res.(float64)
+	}
+	return float64(0)
 }
---
+++ grabbag_test.go
@@ -0,0 +1,100 @@
+package grabbag
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestGrabBagFromData(t *testing.T) {
+	t.Parallel()
+	gb := FromData(sampleData())
+	assert.NotNil(t, gb, "FromData gave us nil")
+}
+
+func TestGrabBag_Grab(t *testing.T) {
+	t.Parallel()
+	gb := FromData(sampleData())
+	result := gb.Grab("hello")
+	assert.NotNil(t, result, "Grab returned nil")
+}
+
+func TestGrabBag_Has(t *testing.T) {
+	t.Parallel()
+	gb := FromData(sampleData())
+	result := gb.Has("hello")
+	assert.Equal(t, result, true, "Grab bag returned a false negative for has")
+
+	result = gb.Has("hello.someOther.field")
+	assert.Equal(t, result, false, "Grab bag returned a false positive for has")
+}
+
+func TestGrabBag_Nested(t *testing.T) {
+	t.Parallel()
+	gb := FromData(sampleData())
+	result := gb.String("nested.hello")
+	assert.Equal(t, "world inner", result, "Grab bag did not return proper string")
+}
+
+func TestGrabBag_Types(t *testing.T) {
+	t.Parallel()
+	gb := FromData(sampleData())
+
+	str := gb.String("hello")
+	assert.Equal(t, "world", str, "Grab bag did not return proper string")
+
+	i := gb.Int("int")
+	assert.Equal(t, 1, i, "Grab bag did not return proper int")
+
+	f32 := gb.Float32("float32")
+	assert.Equal(t, float32(1), f32, "Grab bag did not return proper float32")
+
+	f64 := gb.Float64("float64")
+	assert.Equal(t, float64(1), f64, "Grab bag did not return proper float64")
+
+	b := gb.Bool("bool")
+	assert.Equal(t, true, b, "Grab bag did not return proper bool")
+
+	intSlice := gb.IntSlice("intSlice")
+	assert.Len(t, intSlice, 5, "Grab bag missed some ints in our slice")
+	assert.Equal(t, intSlice, []int{1, 2, 3, 4, 5}, "Grab bag did not return proper int slice")
+
+	strSlice := gb.StringSlice("stringSlice")
+	assert.Len(t, strSlice, 2, "Grab bag missed some strings in our slice")
+	assert.Equal(t, strSlice, []string{"hello", "world"}, "Grab bag did not return proper string slice")
+}
+
+func BenchmarkGrabBag_Simple(b *testing.B) {
+	gb := FromData(sampleData())
+
+	for i := 0; i < b.N; i++ {
+		gb.Grab("hello")
+	}
+}
+
+func BenchmarkGrabBag_Traverse(b *testing.B) {
+	gb := FromData(sampleData())
+
+	for i := 0; i < b.N; i++ {
+		gb.Grab("nested.inner.another.value")
+	}
+}
+
+func sampleData() map[string]interface{} {
+	return map[string]interface{}{
+		"hello":       "world",
+		"int":         1,
+		"float32":     float32(1),
+		"float64":     float64(1),
+		"bool":        true,
+		"intSlice":    []int{1, 2, 3, 4, 5},
+		"stringSlice": []string{"hello", "world"},
+		"nested": map[string]interface{}{
+			"hello": "world inner",
+			"inner": map[string]interface{}{
+				"another": map[string]interface{}{
+					"value": 5,
+				},
+			},
+		},
+	}
+}
